9474811c006b53e8f9ae70a3ab6d5d11c2dae3a8
Md Abu Talha Mian <sma.talha@yahoo.com>
2020-04-10 06:24:52 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,7 +1,11 @@
 def first_challenge
-    epic_tragedy = {
-      :montague => {},
-      :capulet => {}
-    }
+   epic_tragedy = {
+     :montague => {},
+     :capulet => {}
+   }
 end
 
+epic_tragedy = {
+  :montague => {},
+  :capulet => {}
+}
